c1b6e529142f82510a94992ad69d2b80af90b970
Iktiup-Mahmud <seyam.official96@gmail.com>
2023-04-15 02:42:21 +0600

    roughly-done

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
-const app = express()
+const cors = require('cors')
+require('colors')
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
 
 const port = process.env.PORT || 5000
 
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+async function run() {
+
+}
+
+run().catch(console.log())
 
 app.get('/', (req, res) => {
     res.send('Server is UPP & runnning')
 })
-
-
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`)
